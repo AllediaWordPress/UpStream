6fde8028fdce9d4f717ad183d60ece250c5d0785
b-trav <ben@travaglione.com>
2018-07-04 09:56:40 +0800

    On the front-end projects page, projects are now sorted alphabetically.

--- src/templates/archive-project.php
+++ src/templates/archive-project.php
@@ -111,6 +111,13 @@ if ( isset( $currentUser->projects ) ) {
     unset( $currentUser->projects );
 }
 
+//Modified by Ben Travaglione 2018-07-04
+//Sort the projectsList alphabetically by 'slug'
+function cmp($a, $b) {
+    return ($a->slug < $b->slug) ? -1 : 1;
+}
+uasort($projectsList, 'cmp'); 
+
 $projectsListCount = count( $projectsList );
 
 upstream_get_template_part( 'global/header.php' );
