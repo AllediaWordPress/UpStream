5bf15afb6b332e40ea82e0443f26dcdabffbca31
andergmartins <anderson@alledia.com>
2019-05-30 17:19:32 -0300

    Update to stable version 1.24.3

--- upstream.php
+++ upstream.php
@@ -4,7 +4,7 @@
  * Description: A WordPress Project Management plugin by UpStream.
  * Author: UpStream
  * Author URI: https://upstreamplugin.com
- * Version: 1.24.3-beta.5
+ * Version: 1.24.3
  * Text Domain: upstream
  * Domain Path: /languages
  */
