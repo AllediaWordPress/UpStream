74e150154684c6e20349aefcc8582500b1874def
westbourneintl <raj@westbourneinternational.com>
2020-05-25 21:17:25 -0400

    release 1.30.0

--- includes.php
+++ includes.php
@@ -1,2 +1,2 @@
 <?php
-define('UPSTREAM_VERSION', '1.30.0');
+define('UPSTREAM_VERSION', '1.32.0');
--- upstream.php
+++ upstream.php
@@ -4,7 +4,7 @@
  * Description: A WordPress Project Management plugin by UpStream.
  * Author: UpStream
  * Author URI: https://upstreamplugin.com
- * Version: 1.31.0
+ * Version: 1.32.0
  * Text Domain: upstream
  * Domain Path: /languages
  */
